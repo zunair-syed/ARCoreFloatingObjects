f6f3d3d52080b86ab65f23c9eca513d439395151
ZUNAIR SYED <zu2syed@uwaterloo.ca>
2017-12-04 22:34:20 -0500

    initial commit

---
+++ project/arcore_floating_objects/app/src/main/java/me/zunair/syed/ar/core/floating/objects/FloatingObjectsArActivity.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.zunair.syed.ar.core.floating.objects;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Frame.TrackingState;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+import me.zunair.syed.ar.core.floating.objects.rendering.BackgroundRenderer;
+import me.zunair.syed.ar.core.floating.objects.rendering.ObjectRenderer;
+import me.zunair.syed.ar.core.floating.objects.rendering.ObjectRenderer.BlendMode;
+import me.zunair.syed.ar.core.floating.objects.rendering.PlaneAttachment;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+
+public class FloatingObjectsArActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
+    private static final String TAG = FloatingObjectsArActivity.class.getSimpleName();
+
+    // Rendering. The Renderers are created here, and initialized when the GL surface is created.
+    private GLSurfaceView mSurfaceView;
+
+    private Config mDefaultConfig;
+    private Session mSession;
+    private BackgroundRenderer mBackgroundRenderer = new BackgroundRenderer();
+    private GestureDetector mGestureDetector;
+
+    private ObjectRenderer mVirtualObject = new ObjectRenderer();
+    private ObjectRenderer mVirtualObjectShadow = new ObjectRenderer();
+
+    // Temporary matrix allocated here to reduce number of allocations for each frame.
+    private final float[] mAnchorMatrix = new float[16];
+
+    // Tap handling and UI.
+    private ArrayBlockingQueue<MotionEvent> mQueuedSingleTaps = new ArrayBlockingQueue<>(16);
+    private ArrayList<PlaneAttachment> mTouches = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSurfaceView = (GLSurfaceView) findViewById(R.id.surfaceview);
+
+        mSession = new Session(/*context=*/this);
+
+        // Create default config, check is supported, create session from that config.
+        mDefaultConfig = Config.createDefaultConfig();
+        if (!mSession.isSupported(mDefaultConfig)) {
+            Toast.makeText(this, "This device does not support AR", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        // Set up tap listener.
+        mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                onSingleTap(e);
+                return true;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+        });
+
+        mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return mGestureDetector.onTouchEvent(event);
+            }
+        });
+
+        // Set up renderer.
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
+        mSurfaceView.setRenderer(this);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+        // permission on Android M and above, now is a good time to ask the user for it.
+        if (CameraPermissionHelper.hasCameraPermission(this)) {
+
+            // Note that order matters - see the note in onPause(), the reverse applies here.
+            mSession.resume(mDefaultConfig);
+            mSurfaceView.onResume();
+        } else {
+            CameraPermissionHelper.requestCameraPermission(this);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Note that the order matters - GLSurfaceView is paused first so that it does not try
+        // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+        // still call mSession.update() and get a SessionPausedException.
+        mSurfaceView.onPause();
+        mSession.pause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this,
+                "Camera permission is needed to run this application", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Standard Android full-screen functionality.
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    private void onSingleTap(MotionEvent e) {
+        // Queue tap if there is space. Tap is lost if queue is full.
+        mQueuedSingleTaps.offer(e);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        // Create the texture and pass it to ARCore session to be filled during update().
+        mBackgroundRenderer.createOnGlThread(/*context=*/this);
+        mSession.setCameraTextureName(mBackgroundRenderer.getTextureId());
+
+        // Prepare the other rendering objects.
+        try {
+            mVirtualObject.createOnGlThread(/*context=*/this, "andy.obj", "andy.png");
+            mVirtualObject.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+
+            mVirtualObjectShadow.createOnGlThread(/*context=*/this,
+                "andy_shadow.obj", "andy_shadow.png");
+            mVirtualObjectShadow.setBlendMode(BlendMode.Shadow);
+            mVirtualObjectShadow.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read obj file");
+        }
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+        // Notify ARCore session that the view size changed so that the perspective matrix and
+        // the video background can be properly adjusted.
+        mSession.setDisplayGeometry(width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clear screen to notify driver it should not load any pixels from previous frame.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        try {
+            // Obtain the current frame from ARSession. When the configuration is set to
+            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+            // camera framerate.
+            Frame frame = mSession.update();
+
+            // Handle taps. Handling only one tap per frame, as taps are usually low frequency
+            // compared to frame rate.
+            MotionEvent tap = mQueuedSingleTaps.poll();
+
+            if(tap!=null && frame.getTrackingState() == TrackingState.TRACKING){
+
+                Log.d(TAG, "X and Y : (" +tap.getX() + ", " + tap.getY()+")");
+                Log.d(TAG, "RAW X and Y : (" +tap.getRawX() + ", " + tap.getRawY()+")");
+
+                float[] touchArray = new float[]{tap.getX(), tap.getY()};
+                float objectDistanceFromCamera = -1.0f;
+                float[] normalizedMetersArr = getNormalizedScreenCoordinates(touchArray, mSurfaceView.getWidth(), mSurfaceView.getHeight(), objectDistanceFromCamera);
+
+                Pose inAirPose = frame.getPose().compose(Pose.makeTranslation(normalizedMetersArr[0], normalizedMetersArr[1], objectDistanceFromCamera)).extractTranslation();
+                mSession.addAnchor(inAirPose);
+            }
+
+            // Draw background.
+            mBackgroundRenderer.draw(frame);
+
+            // If not tracking, don't draw 3d objects.
+            if (frame.getTrackingState() == TrackingState.NOT_TRACKING) {
+                return;
+            }
+
+            // Get projection matrix.
+            float[] projmtx = new float[16];
+            mSession.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f);
+
+            // Get camera matrix and draw.
+            float[] viewmtx = new float[16];
+            frame.getViewMatrix(viewmtx, 0);
+
+            // Compute lighting from average intensity of the image.
+            final float lightIntensity = frame.getLightEstimate().getPixelIntensity();
+
+            // Visualize anchors created by touch.
+            float scaleFactor = 1.0f;
+            for(Anchor a : mSession.getAllAnchors()){
+
+                a.getPose().toMatrix(mAnchorMatrix, 0);
+
+                // Update and draw the model and its shadow.
+                mVirtualObject.updateModelMatrix(mAnchorMatrix, scaleFactor);
+                mVirtualObjectShadow.updateModelMatrix(mAnchorMatrix, scaleFactor);
+                mVirtualObject.draw(viewmtx, projmtx, lightIntensity);
+                mVirtualObjectShadow.draw(viewmtx, projmtx, lightIntensity);
+            }
+
+        } catch (Throwable t) {
+            // Avoid crashing the application due to unhandled exceptions.
+            Log.e(TAG, "Exception on the OpenGL thread", t);
+        }
+    }
+
+
+    private float [] getNormalizedScreenCoordinates(float[] vec2, int screenWidth, int screenHeight, float metersAway){
+        if(vec2 == null || vec2.length != 2) return null;
+
+        float [] normalizedTouch = new float[]{vec2[0]/screenWidth, vec2[1]/screenHeight};
+        float screenWidthInTranslationMeters = 0.37f; //range is [-0.37,0.37]
+        float screenHeightInTranslationMeters = 0.675f; //range is [-0.615, 0.615]
+
+        float normalizedAwayFromCenterX;
+        float normalizedMetersAwayFromCenterX;
+        if(normalizedTouch[0] >= 0.5f){
+            normalizedAwayFromCenterX = (float) ((1.0 - 0.0) / (1.0 - 0.5f) * (normalizedTouch[0] - 1.0f) + 1.0f);
+        }else{
+            normalizedAwayFromCenterX = (float) ((0.0 + 1.0) / (0.5f - 0f) * (normalizedTouch[0] - 1.0f) + 1.0f);
+        }
+        normalizedMetersAwayFromCenterX = normalizedAwayFromCenterX * screenWidthInTranslationMeters * Math.abs(metersAway);
+
+
+        float normalizedAwayFromCenterY ;
+        float normalizedMetersAwayFromCenterY;
+        if((1-normalizedTouch[1]) < 0.5f){
+            normalizedAwayFromCenterY = (float) ((1.0 - 0.0) / (1.0 - 0.5f) * ((1 - normalizedTouch[1]) - 1.0f) + 1.0f);
+        }else{
+            normalizedAwayFromCenterY = (float) ((0.0 + 1.0) / (0.5f - 0f) * ((1 - normalizedTouch[1]) - 1.0f) + 1.0f);
+        }
+        normalizedMetersAwayFromCenterY = normalizedAwayFromCenterY * screenHeightInTranslationMeters * Math.abs(metersAway);
+
+        return new float[]{normalizedMetersAwayFromCenterX, normalizedMetersAwayFromCenterY};
+    }
+
+}
